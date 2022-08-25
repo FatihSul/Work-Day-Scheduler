6dcc3492f0ded2f70bdc851ca97d94e6c92c0afd
FatihSul <fatih9720@hotmail.com>
2022-08-25 18:43:05 -0400

    finished site

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Work-Day-Scheduler
+
+A website to plan out your schedule for the day by the hour. Updates in real time for the date and color coordinated by what hour you are on as well as past and future!
+
+![preview of the webite of input boxes per hour from 9-5](./Work-Day-Scheduler.png)
---
+++ script.js
@@ -0,0 +1,46 @@
+const currentTime = moment()
+const jumboTime = moment().format("dddd, MMMM Do");
+document.getElementById("currentDay").innerHTML = jumboTime;
+//sets the time shown on the jumbotron
+
+
+
+//gives the colored backgrounds according to time of day 
+$(".time-block").each(function(){
+    const hour = parseInt($(this).attr("id"));
+
+    if (currentTime < hour){
+        $(this).addclass("future");
+    } else if (currentTime === hour){
+
+        $(this).addClass("present");
+    } else {
+
+        $(this).addClass("past")
+    }
+
+})
+
+
+//for the save button to save the text within the blocks
+$(".saveBtn").on("click", function(){
+    var blockNo = $(this).parent().attr("id");
+    var blockText = $(this).siblings(".col-10").val();
+    localStorage.setItem(blockNo, blockText);
+    console.log("test")
+})
+
+
+
+// loads the localstorage for the text within the blocks on page load
+window.onload = function(){
+    $("#No9Txt").val(localStorage.getItem("9"));
+    $("#No10Txt").val(localStorage.getItem("10"));
+    $("#No11Txt").val(localStorage.getItem("11"));
+    $("#No12Txt").val(localStorage.getItem("12"));
+    $("#No1Txt").val(localStorage.getItem("1"));
+    $("#No2Txt").val(localStorage.getItem("2"));
+    $("#No3Txt").val(localStorage.getItem("3"));
+    $("#No4Txt").val(localStorage.getItem("4"));
+    $("#No5Txt").val(localStorage.getItem("5"));
+}
